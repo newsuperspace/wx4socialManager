34f01ee2c32c9b09f4d3d31776648cd87fa7a7d7
newsuperspace <459464207@qq.com>
2018-02-21 17:57:07 +0800

    完成了DoingProject和BesureProject部分的数据库ORM

---
+++ src/cc/natapp4/ddaig/domain/ProjectAboutShoping.java
@@ -0,0 +1,17 @@
+package cc.natapp4.ddaig.domain;
+
+import java.io.Serializable;
+
+/**
+ * TODO 待完善
+ * 这个类作为DoingProject的一个子部分存在
+ * 用来管理项目采购的资金使用、
+ * @author Administrator
+ *
+ */
+public class ProjectAboutShoping implements Serializable {
+
+	// 【主键】
+	private String pasid;
+	
+}
--- src/cc/natapp4/ddaig/domain/Receipt4BesureProject.java
+++ src/cc/natapp4/ddaig/domain/Receipt4BesureProject.java
@@ -12,7 +12,7 @@ public class Receipt4BesureProject implements Serializable {
 	// 【主键】——建议主键产生器使用sequence，节省空间消耗
 	private String rbpid;
 	// 回复人姓名
-	private String rname;
+	private String name;
 	/*
 	 * 信息类型（当前硬编码至此）
 	 * succeed: 通过审核
@@ -26,6 +26,45 @@ public class Receipt4BesureProject implements Serializable {
 	private BesureProject  besureProject;
 	// 回复时间 2018-1-23 13:32:01（格里高利历毫秒值偏移）
 	private long time;
+
+	// ==============SETTERs/GETTERs================
+	public String getRbpid() {
+		return rbpid;
+	}
+	public void setRbpid(String rbpid) {
+		this.rbpid = rbpid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public BesureProject getBesureProject() {
+		return besureProject;
+	}
+	public void setBesureProject(BesureProject besureProject) {
+		this.besureProject = besureProject;
+	}
+	public long getTime() {
+		return time;
+	}
+	public void setTime(long time) {
+		this.time = time;
+	}
 	
 	
 	
