42c277cfdc3b2c07ecbc1c4d7b60728cf9628c36
newsuperspace <459464207@qq.com>
2019-06-04 18:38:41 +0800

    【bug】修复了createActivity.jsp中FullCalendar突然不能使用的bug
    （1）createActivity.jsp中FullCalendar突然不能使用，是因为在页面初始化时会通过jQuery向服务器端（activityAction类）获取getStartDateAndEndDate()的数据信息，该方法中会调用ActivityUtils中的方法，不知合规原本获取今天的星期所返回字符串时“星期一、星期二、星期三。。。” 但现在返回的是“周一、周二、周三。。。。”，因此我稍微在case语句中增加了“||"周一".equals(week)”以适应本次变化。
    （2）修改了fullcalendar.css样式表在日历中选择时间段的颜色，更加鲜明。

--- src/cc/natapp4/ddaig/utils/ActivityUtils.java
+++ src/cc/natapp4/ddaig/utils/ActivityUtils.java
@@ -23,46 +23,46 @@ public class ActivityUtils {
 			// TODO 待定
 			break;
 		case 0:	// 社区
-			if("星期一".equals(week)){
+			if("星期一".equals(week)||"周一".equals(week)){
 				// 星期二到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 6;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期二".equals(week)){
+			}else if("星期二".equals(week)||"周二".equals(week)){
 				// 星期三到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 5;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期三".equals(week)){
+			}else if("星期三".equals(week)||"周三".equals(week)){
 				// 社区每周三可以开启下周的预约了
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 11;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期四".equals(week)){
+			}else if("星期四".equals(week)||"周四".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 10;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期五".equals(week)){
+			}else if("星期五".equals(week)||"周五".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 9;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期六".equals(week)){
+			}else if("星期六".equals(week)||"周六".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 8;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期日".equals(week)){
+			}else if("星期日".equals(week)||"周日".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 7;
@@ -71,47 +71,47 @@ public class ActivityUtils {
 			}
 			break;
 		case 1:	// 第一
-			if("星期一".equals(week)){
+			if("星期一".equals(week)||"周一".equals(week)){
 				// 星期二到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 6;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期二".equals(week)){
+			}else if("星期二".equals(week)||"周二".equals(week)){
 				// 星期三到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 5;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期三".equals(week)){
+			}else if("星期三".equals(week)||"周三".equals(week)){
 				// 星期四到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 4;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期四".equals(week)){
+			}else if("星期四".equals(week)||"周四".equals(week)){
 				// 第一层级可在周四开启下周预约
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 10;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期五".equals(week)){
+			}else if("星期五".equals(week)||"周五".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 9;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期六".equals(week)){
+			}else if("星期六".equals(week)||"周六".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 8;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期日".equals(week)){
+			}else if("星期日".equals(week)||"周日".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 7;
@@ -120,48 +120,48 @@ public class ActivityUtils {
 			}
 			break;
 		case 2:	// 第二
-			if("星期一".equals(week)){
+			if("星期一".equals(week)||"周一".equals(week)){
 				// 星期二到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 6;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期二".equals(week)){
+			}else if("星期二".equals(week)||"周二".equals(week)){
 				// 星期三到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 5;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期三".equals(week)){
+			}else if("星期三".equals(week)||"周三".equals(week)){
 				// 星期四到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 4;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期四".equals(week)){
+			}else if("星期四".equals(week)||"周四".equals(week)){
 				// 星期五到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 3;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期五".equals(week)){
+			}else if("星期五".equals(week)||"周五".equals(week)){
 				// 周五就可以提前安排下周活动
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 9;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期六".equals(week)){
+			}else if("星期六".equals(week)||"周六".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 8;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期日".equals(week)){
+			}else if("星期日".equals(week)||"周日".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 7;
@@ -170,49 +170,49 @@ public class ActivityUtils {
 			}
 			break;
 		case 3:	// 第三
-			if("星期一".equals(week)){
+			if("星期一".equals(week)||"周一".equals(week)){
 				// 星期二到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 6;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期二".equals(week)){
+			}else if("星期二".equals(week)||"周二".equals(week)){
 				// 星期三到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 5;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期三".equals(week)){
+			}else if("星期三".equals(week)||"周三".equals(week)){
 				// 星期四到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 4;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期四".equals(week)){
+			}else if("星期四".equals(week)||"周四".equals(week)){
 				// 星期五到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 3;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期五".equals(week)){
+			}else if("星期五".equals(week)||"周五".equals(week)){
 				// 星期六到星期日
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 2;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期六".equals(week)){
+			}else if("星期六".equals(week)||"周六".equals(week)){
 				// 第三层级周六可以安排下周活动
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 8;
 				result.setStartDay(startDay);
 				result.setEndDay(endDay);
-			}else if("星期日".equals(week)){
+			}else if("星期日".equals(week)||"周日".equals(week)){
 				million = currentMillion + 1000L*60*60*24;;
 				startDay = format.format(new Date(million));
 				endDay  = 7;
